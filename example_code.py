15c79c098a1fb6f05371ad7e8f33d65722950ac5
matscorse <matsco@bas.ac.uk>
2024-01-12 12:12:18 +0000

    changed array dimensions and added end comment

--- example_code.py
+++ example_code.py
@@ -6,7 +6,7 @@ warnings.filterwarnings('ignore')
 ##xr.set_options(display_expand_data=False);
 
 # -- numpy array 
-sst_np = np.random.rand(300,450)
+sst_np = np.random.rand(400,450)
 type(sst_np)
 
 sst_da = da.from_array( sst_np)
@@ -22,7 +22,7 @@ sst_xr = xr.DataArray(sst_da,dims=['lat','lon'])
 print(sst_xr.dims)
 
 # -- create some dummy values for lat and lon dimensions
-lat = np.random.uniform(low=-90, high=90, size=300)
+lat = np.random.uniform(low=-90, high=90, size=400)
 lon = np.random.uniform(low=-180, high=180, size=450)
 
 sst_xr = xr.DataArray(sst_da,
@@ -33,3 +33,5 @@ sst_xr = xr.DataArray(sst_da,
                         units="degC")
                      )
 print(sst_xr)
+
+# this is a comment at the end of the file
